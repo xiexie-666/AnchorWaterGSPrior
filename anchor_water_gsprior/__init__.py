3778ad055e96da995786efde828b78fed7986683
xiexie-666 <xietianci@mail.nwpu.edu.cn>
2026-09-20 23:57:04 +0800

    Add package exports

---
+++ anchor_water_gsprior/__init__.py
@@ -0,0 +1,6 @@
+"""AnchorWaterGSPrior: geometry-first underwater Gaussian reconstruction."""
+
+from .config import TrainConfig
+from .model import AnchorWaterModel
+
+__all__ = ["TrainConfig", "AnchorWaterModel"]
